b0ef2ca41e2eee44f10ce6c63954070ffdb9910d
Kevin <crochedengren@gmail.com>
2023-09-17 23:55:07 -0700

    Added js comments

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,23 @@ const questions = [
         correct: "Oshawott"
     },
     {
-        question: "who is best boy",
-        answer: ["Lupin III", "Goemon", "Jigen", "zenigata"],
-        correct: "Snivy"
+        question: "Who is best boy",
+        answer: ["Lupin III", "Goemon", "Jigen", "Zenigata"],
+        correct: "Goemon"
     },
     {
-        question: "who is the worstPokemon",
-        answer: ["Oshawott", "Snivy", "Mudkip", "Charmander"],
-        correct: "Snivy"
+        question: "What is 5 x 6 + 20",
+        answer: ["13", "20", "50", "56"],
+        correct: "50"
     }
 ]
+
 let images = [
     "./assets/img/Oshawott-PNG-Isolated-Image.png",
     "./assets/img/hiclipart.com.png",
     "./assets/img/pngimg.com - number1_PNG14895.png"
 ]
+
 let questionIndex = 0
 let answerButton = document.querySelectorAll(".btn")
 let start = $("#start-btn")
@@ -34,14 +36,14 @@ let time = 25
 let listScores = $("#scores")
 let setImg = $("#q-a img")
 
-function startGame() {
+function startGame() { //starts quiz
     questionIndex = 0
     score = 0
     loadQuestion()
     timer()
 }
 
-function timer() {
+function timer() { // starts timer
     timeInterval = setInterval(function () {
         if (time >= 1) {
             time--
@@ -55,18 +57,18 @@ function timer() {
     }, 1000)
 }
 
-function loadQuestion() {
-    start.addClass("hide");
-    qa.removeClass("hide");
-    questionTitle.text(questions[questionIndex].question);
+function loadQuestion() { // loads quetions
+    start.addClass("hide"); // hides start button
+    qa.removeClass("hide"); // shows quiz
+    questionTitle.text(questions[questionIndex].question); // gets question title
     answerButton.forEach((button) => {
-        button.removeEventListener("click", checkAnswer);
+        button.removeEventListener("click", checkAnswer); // remove eventlisteners stop each button to have more than one
     });
-    setImg.attr("src", images[questionIndex])
+    setImg.attr("src", images[questionIndex]) // sets img based on question[index]
     answerButton.forEach((button, index) => {
-        let answer = questions[questionIndex].answer[index];
+        let answer = questions[questionIndex].answer[index]; // pulls all possible answers
         button.textContent = answer;
-        button.disabled = false;
+        button.disabled = false; // turn buttons on for user
         $(button).removeClass("correct wrong");
         button.addEventListener("click", checkAnswer);
     });
@@ -74,10 +76,10 @@ function loadQuestion() {
 
 function checkAnswer() {
     answerButton.forEach((button) => {
-        button.disabled = true;
+        button.disabled = true; // stops user from selecting more than one answer
     });
     let answer = this.textContent;
-    if (questions[questionIndex].correct === answer) {
+    if (questions[questionIndex].correct === answer) { // checks to see if answer is right
         $(this).addClass("correct");
         score++;
         console.log(score);
@@ -86,13 +88,13 @@ function checkAnswer() {
         time -= 5
     }
     answerButton.forEach((button, index) => {
-        if (questions[questionIndex].correct === questions[questionIndex].answer[index]) {
+        if (questions[questionIndex].correct === questions[questionIndex].answer[index]) { // this will show correct answer after use selects an answer
             $(button).addClass("correct");
         }
     });
 }
 
-function nextQuestion() {
+function nextQuestion() { // starts next question
     questionIndex++
     if (questionIndex < questions.length) {
         loadQuestion()
@@ -106,23 +108,23 @@ function endGame() {
     timeLeft.text(0)
     start.removeClass("hide");
     qa.addClass("hide");
-    const highscores = {
+    const highscores = { // logs score in a object
         name: prompt(score + "/" + questions.length + ", Good job, what name would you like to save your score as?"),
         score: score
     }
     scoreBoard.push(highscores)
-    localStorage.setItem("scoreboard", JSON.stringify(scoreBoard))
+    localStorage.setItem("scoreboard", JSON.stringify(scoreBoard)) // saves scores to local storage
     displayLeaderboard()
 }
 
 function displayLeaderboard() {
-    const storedScores = localStorage.getItem("scoreboard")
+    const storedScores = localStorage.getItem("scoreboard") // gets scores from local storage
     if (storedScores) {
         scoreBoard = JSON.parse(storedScores)
-        scoreBoard.sort((a, b) => b.score - a.score)
+        scoreBoard.sort((a, b) => b.score - a.score)// sorts objects by scores highest to lowest
         console.log(scoreBoard)
-        listScores.empty()
-        for (index of scoreBoard) {
+        listScores.empty() // clears previous board to stop duplicates when showing new score
+        for (index of scoreBoard) { // displays highscores
             newScore = $("<li>").text(index.name + ": " + index.score)
             listScores.append(newScore)
         }
